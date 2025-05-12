abcb3d938685a39053c4a4b742039ad537acc885
Armaankhaan01 <armankhan876426@gmail.com>
2025-05-13 00:47:45 +0530

    fix: .env error

--- scripts/build-sw.js
+++ scripts/build-sw.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const template = fs.readFileSync("firebase-messaging-sw.template.js", "utf8");
 
 const result = template
-  .replace("__API_KEY__", process.env.FIREBASE_API_KEY)
-  .replace("__AUTH_DOMAIN__", process.env.FIREBASE_AUTH_DOMAIN)
-  .replace("__PROJECT_ID__", process.env.FIREBASE_PROJECT_ID)
-  .replace("__STORAGE_BUCKET__", process.env.FIREBASE_STORAGE_BUCKET)
-  .replace("__SENDER_ID__", process.env.FIREBASE_SENDER_ID)
-  .replace("__APP_ID__", process.env.FIREBASE_APP_ID)
-  .replace("__MEASUREMENT_ID__", process.env.FIREBASE_MEASUREMENT_ID);
+  .replace("__API_KEY__", process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
+  .replace("__AUTH_DOMAIN__", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN)
+  .replace("__PROJECT_ID__", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID)
+  .replace("__STORAGE_BUCKET__", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET)
+  .replace("__SENDER_ID__", process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID)
+  .replace("__APP_ID__", process.env.NEXT_PUBLIC_FIREBASE_APP_ID)
+  .replace("__MEASUREMENT_ID__", process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID);
 
 fs.writeFileSync("public/firebase-messaging-sw.js", result);
